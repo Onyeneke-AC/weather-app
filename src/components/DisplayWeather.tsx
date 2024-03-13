62f3a42cfb928d0ea7a78a3140555ee51d6bd4e6
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-03-13 21:16:31 +0100

    html finalization

--- src/components/DisplayWeather.tsx
+++ src/components/DisplayWeather.tsx
@@ -2,19 +2,21 @@ import React from 'react'
 import { WeatherWrapper } from './styles.module'
 import { AiOutlineSearch } from 'react-icons/ai';
 import { WiHumidity } from 'react-icons/wi';
+import { SiWindicss } from 'react-icons/si';
 
 function DisplayWeather() {
   return (
     <WeatherWrapper>
       <div className="container">
+
         <div className="searchArea">
             <input type="text" placeholder="Search City..." />
-
             <div className="searchCircle">
                 <AiOutlineSearch className='searchIcon'/>
             </div>
         </div>
-        <div className="wearherArea">
+        
+        <div className="weatherArea">
             <h1>Auckland</h1>
             <span>Nz</span>
             <div className="icon">
@@ -28,7 +30,15 @@ function DisplayWeather() {
             <div className="humidityLevel">
                 <WiHumidity className='windIcon'/>
                 <div className="humidInfo">
-                    
+                    <h1>60%</h1>
+                    <p>Humidity</p> 
+                </div>
+            </div>
+            <div className="wind">
+                <SiWindicss className="windIcon"/>
+                <div className="humidInfo">
+                    <h1>2.35km</h1>
+                    <p>Wind Speed</p> 
                 </div>
             </div>
         </div>
