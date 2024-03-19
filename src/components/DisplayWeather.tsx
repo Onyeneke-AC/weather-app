c64c7dc3bebca48fc0fdb64bdf205821bc97c277
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-03-19 08:39:44 +0100

    useRef update to the search component

--- src/components/DisplayWeather.tsx
+++ src/components/DisplayWeather.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { WeatherWrapper } from './styles.module'
 import { AiOutlineSearch } from 'react-icons/ai';
 import { WiHumidity } from 'react-icons/wi';
@@ -12,26 +12,45 @@ function DisplayWeather() {
 
     const [weatherData, setWeatherData] = React.useState<WeatherDataProps | null>(null);
     const [isLoading, setIsLoading] = React.useState(false);
-    const [searchCity, setSearchCity] = React.useState("");
+    // const [searchCity, setSearchCity] = React.useState("");
     const [place, setPlace] = React.useState("");
     const [lat, setLat] = React.useState<number>();
     const [lon, setLon] = React.useState<number>();
     const sky = useSearchWeather(place);
     const geo = useLatLonWeather(lat, lon);
+    const searchRef = useRef<HTMLInputElement>(null);
+
+    // const handleSearch = () => {
+    //     if (searchCity.trim() === "" ){
+    //         return;
+    //     }
+    //     setPlace(searchCity);
+    //     try {
+    //         if(!sky.isLoading && sky.data !== undefined){
+    //             setWeatherData(sky.data);
+    //         } 
+    //     }catch (error) {
+    //         console.error("No results found");
+    //     }
+    // }
 
     const handleSearch = () => {
-        if (searchCity.trim() === "" ){
-            return;
-        }
-        setPlace(searchCity);
-        try {
-            if(!sky.isLoading && sky.data !== undefined){
-                setWeatherData(sky.data);
-            } 
-        }catch (error) {
-            console.error("No results found");
+        if (searchRef.current) {
+            const searchCity = searchRef.current.value;
+            if (searchCity.trim() === "" ){
+                return;
+            }
+            setPlace(searchCity);
+            try {
+                if(!sky.isLoading && sky.data !== undefined){
+                    setWeatherData(sky.data);
+                } 
+            }catch (error) {
+                console.error("No results found");
+            }
         }
     }
+
 
 
     React.useEffect(() => {
@@ -56,8 +75,8 @@ function DisplayWeather() {
       <div className="container">
 
         <div className="searchArea">
-            <input type="text" placeholder="Search City..." value={searchCity} onChange={
-                (e) => setSearchCity(e.target.value)}/>
+            { <input type="text" placeholder="Search City..." ref={searchRef}/*value={searchCity} onChange={
+                (e) => setSearchCity(e.target.value)}*//> }
             <div className="searchCircle">
                 <AiOutlineSearch className='searchIcon' onClick={handleSearch}/>
             </div>
